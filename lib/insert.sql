00c158751e4aeaaa1054c3e0fe4a5484d09158cb
otienoonyango2000 <otienoonyango2000@gmail.com>
2023-02-18 10:55:03 +0300

    sql first commit bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+    VALUES("Mr. Chocolate", 56, "M", "Red", "High", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive)    
+    VALUES("Rowdy", 8, "M", "Green", "Low", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive)    
+    VALUES("Tabitha", 35, "F", "Violet", "Medium", false);
+INSERT INTO bears(name, age, sex, color, temperament, alive)    
+    VALUES("Sergeant Brown", 20, "M", "Green", "Low", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive)    
+    VALUES("Melissa", 46, "F", "Red", "High", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive)    
+    VALUES("Grinch", 70, "F", "Green", "Low", false);
+INSERT INTO bears(name, age, sex, color, temperament, alive)    
+    VALUES("Wendy", 21, "F", "Green", "Low", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive)    
+    VALUES(NULL, 75, "M", "Blue", "Medium", true);
+    
